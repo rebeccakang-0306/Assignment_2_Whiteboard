2cb5be36abe2f7e943c370b9350a76d5bc5f4d14
yinankang <yinank@student.unimelb.edu.au>
2021-05-11 11:03:49 +0800

    Initial commit

---
+++ src/Line.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Line extends DrawService {
+    void draw(Graphics2D g2d) {
+        g2d.setStroke(new BasicStroke(1.0f,
+                BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
+        g2d.drawLine(x1, y1, x2, y2);
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Rectangle extends DrawService {
+    void draw(Graphics2D g2d) {
+        g2d.setStroke(new BasicStroke(1.0f));
+        g2d.drawRect(Math.min(x1, x2), Math.min(y1, y2),
+                Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+}
